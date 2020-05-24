12fbb6d1c06bc396f34e644ed61c0974d58df86b
Slavomir <gagliardetto@users.noreply.github.com>
2020-05-24 19:10:04 +0300

    IsNullable on Basic type is always false

--- scanner/package.go
+++ scanner/package.go
@@ -155,8 +155,21 @@ func NewBasic(name string) Type {
 	}
 }
 
-// IsNullable returns true. Basic types though cannot be nullable, they are considered so in protobuf.
-func (b Basic) IsNullable() bool { return true }
+// IsNullable returns false. Basic types are not nullable,
+// and have just the zero values.
+//
+// Basic types:
+// int, int8, int16, int32, int64
+// uint, uint8, uint16, uint32, uint64
+// uintptr
+// float32, float64
+// byte
+// rune
+// string
+// complex64, complex128
+//
+// See: https://nilsmagnus.github.io/post/nillability-in-go/
+func (b Basic) IsNullable() bool { return false }
 func (b Basic) IsBasic() bool    { return true }
 
 // String returns a string representation for the type
